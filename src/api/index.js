fc89e97e6f0729639eb029f0035b89b4fd6fb405
meltonb <brentmelton99@yahoo.com>
2019-09-25 17:17:38 -0700

    initialization

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,58 @@
-import { version } from '../../package.json';
-import { Router } from 'express';
-import facets from './facets';
+import {
+	version
+} from "../../package.json";
+import {
+	Router
+} from "express";
+import facets from "./facets";
 
-export default ({ config, db }) => {
+// const configuration = require("config"); 
+// console.log("config: ", configuration);
+// console.log("key", process.env.apiKey);
+
+const apiKey = "RGAPI-66285c51-c2d6-4d45-8bd6-ce602d164551";
+const apiKeyParam = `?api_key=${apiKey}`;
+const region = "na1";
+const axios = require("axios").default;
+axios.defaults.baseURL = `https://${region}.api.riotgames.com/lol`;
+
+export default ({
+	config,
+	db
+}) => {
 	let api = Router();
 
 	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+	api.use(
+		"/facets",
+		facets({
+			config,
+			db,
+		})
+	);
 
 	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({ version });
+	api.get("/", async (req, res) => {
+		res.send("Welcome to the backend application!");
+	});
+
+	// get summoner
+	api.get("/summoner/:name", async (req, res) => {
+		const name = req.params.name;
+		if (name) {
+			try {
+				const response = await axios.get(
+					`/summoner/v4/summoners/by-name/${name}${apiKeyParam}`
+				);
+				console.log(response.data);
+				res.send(response.data);
+			} catch (error) {
+				console.error(error);
+				// {"status":{"message":"Data not found - summoner not found","status_code":404}}
+				res.send(error.response.data);
+			}
+		}
 	});
 
 	return api;
-}
+};
