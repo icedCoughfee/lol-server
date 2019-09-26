aa810bbc31eb1b25d56d35023bdd569a12c103d1
icedCoughfee <brentmelton99@yahoo.com>
2019-09-26 16:46:53 -0700

    relocated resource abstraction to it's own module

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,7 @@
 import { version } from "../../package.json";
 import { Router } from "express";
 import facets from "./facets";
-import http from "../services/httpService";
-import _ from "lodash";
-
-require("@babel/polyfill");
-require("dotenv").config();
-
-const apiKey = process.env.apiKey;
-const apiKeyParam = `?api_key=${apiKey}`;
+import apiActions from "./actions";
 
 export default ({ config, db }) => {
   let api = Router();
@@ -27,66 +20,19 @@ export default ({ config, db }) => {
     res.send("Welcome to the backend application!");
   });
 
-  function defaultAction() {}
-
-  getResource(
+  apiActions.getResource(
     api,
     "/summoner/:name",
-    "/summoner/v4/summoners/by-name/{name}",
-    defaultAction,
-    defaultAction
+    "/summoner/v4/summoners/by-name/{name}"
   );
 
   // champion mastery
   // ex FgL6H3nXy2_tJhHGODUgS5stmXU0vPiaaX0EIcnOM8PrTmU/53
-  getResource(
+  apiActions.getResource(
     api,
     "/champion-mastery/:encryptedSummonerId/:championId",
-    "/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}/by-champion/{championId}",
-    defaultAction,
-    defaultAction
+    "/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}/by-champion/{championId}"
   );
-
-  function getUrlParams(req, route) {
-    let urlParams = _.words(route, /:[a-zA-Z0-9]+/g).map(param =>
-      _.replace(param, ":", "")
-    );
-    return urlParams.map(param => ({ param: param, value: req.params[param] }));
-  }
-
-  // given any variation of /lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}/by-champion/{championId}
-  // replace the {variables} with values
-  function createForeignUri(foreignResourceUri, urlParams) {
-    let uri = foreignResourceUri;
-    for (const p of urlParams) {
-      uri = _.replace(uri, `{${p.param}}`, p.value);
-    }
-    return uri;
-  }
-
-  // general get resource abstraction for interfacing with foreign resources
-  function getResource(
-    router,
-    route,
-    foreignResourceUri,
-    onSuccess,
-    onFailure
-  ) {
-    router.get(route, async (req, res) => {
-      const urlParams = getUrlParams(req, route);
-      const uri = createForeignUri(foreignResourceUri, urlParams);
-
-      try {
-        const response = await http.get(`${uri}${apiKeyParam}`);
-        onSuccess();
-        res.send(response.data);
-      } catch (error) {
-        onFailure();
-        // {"status":{"message":"Data not found - summoner not found","status_code":404}}
-        res.send(error.response.data);
-      }
-    });
-  }
 
   return api;
 };
